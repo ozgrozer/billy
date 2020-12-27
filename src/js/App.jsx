da9d360f7a6413b639d68756640e6f28f1947f62
Ozgur <ozgr@live.com>
2020-12-27 05:09:43 +0300

    Save input to cookies

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -11,20 +11,37 @@ const sanitize = input => {
   return text
 }
 
+const writeCookie = props => {
+  const theDate = new Date()
+  const oneYearLater = new Date(theDate.getTime() + 31536000000)
+  const expiryDate = oneYearLater.toGMTString()
+  const value = window.btoa(props.value)
+  document.cookie = props.key + '=' + value + '; expires=' + expiryDate + '; path=/'
+}
+
+const readCookie = props => {
+  const getCookie = document.cookie.match('(^|;)\\s*' + props.key + '\\s*=\\s*([^;]+)')
+
+  if (getCookie) {
+    const lastItem = getCookie.pop()
+    return props.boolean ? lastItem === 'true' : window.atob(lastItem)
+  } else if (props.defaultValue !== undefined) {
+    return props.defaultValue
+  }
+}
+
 const App = () => {
-  const defaultInput = '<div>first = 9</div><div>second = 7</div><div>add = first + second</div><div>multiply = first * second</div><div><br></div><div>first - second</div>'
+  const defaultValue = '<div>first = 9</div><div>second = 7</div><div>add = first + second</div><div>multiply = first * second</div><div><br></div><div>first - second</div>'
+  const defaultInput = readCookie({ key: 'input', defaultValue })
+  const input = sanitize(defaultInput)
+  const defaultOutput = calculate(input)
 
-  const [output, setOutput] = useState()
-
-  useEffect(() => {
-    const input = sanitize(defaultInput)
-    const output = calculate(input)
-    setOutput(output)
-  }, [])
+  const [output, setOutput] = useState(defaultOutput)
 
   const inputOnChange = e => {
     const input = sanitize(e.target.innerHTML)
     const output = calculate(input)
+    writeCookie({ key: 'input', value: input })
     setOutput(output)
   }
 
