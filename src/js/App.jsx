2fa3fd3cc8499fff43cacb8ad658c27284129914
Ozgur <ozgr@live.com>
2021-01-01 23:53:43 +0300

    Add local storage

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -12,7 +12,9 @@ const sanitize = input => {
 }
 
 const App = () => {
-  const defaultInput = '<div>first = 80</div><div>second = 40</div><div><br></div><div>add = first + second</div><div>subtract = first - second</div><div>multiply = first * second</div><div><br></div><div>first / second</div><div>first % second</div>'
+  const localStorageInput = window.localStorage.getItem('input')
+  const _defaultInput = '<div>first = 80</div><div>second = 40</div><div><br></div><div>add = first + second</div><div>subtract = first - second</div><div>multiply = first * second</div><div><br></div><div>first / second</div><div>first % second</div>'
+  const defaultInput = localStorageInput || _defaultInput
   const input = sanitize(defaultInput)
   const defaultOutput = calculate(input)
 
@@ -22,6 +24,7 @@ const App = () => {
     const input = sanitize(e.target.innerHTML)
     const output = calculate(input)
     setOutput(output)
+    window.localStorage.setItem('input', input)
   }
 
   return (
