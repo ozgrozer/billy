b6149255ade030ee8002e04759e5a44120966b69
Ozgur <ozgr@live.com>
2021-01-02 00:04:43 +0300

    No need for sanitization

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -4,24 +4,16 @@ import ReactDOM from 'react-dom'
 import './../css/style.scss'
 import calculate from './calculate'
 
-const sanitize = input => {
-  const div = document.createElement('div')
-  div.innerHTML = input.replace(/<\/div>/g, '</div>\n')
-  const text = div.textContent || div.innerText || ''
-  return text
-}
-
 const App = () => {
   const localStorageInput = window.localStorage.getItem('input')
-  const _defaultInput = '<div>first = 80</div><div>second = 40</div><div><br></div><div>add = first + second</div><div>subtract = first - second</div><div>multiply = first * second</div><div><br></div><div>first / second</div><div>first % second</div>'
+  const _defaultInput = 'first = 80\nsecond = 40\n\nadd = first + second\nsubtract = first - second\nmultiply = first * second\n\nfirst / second\nfirst % second\n'
   const defaultInput = localStorageInput || _defaultInput
-  const input = sanitize(defaultInput)
-  const defaultOutput = calculate(input)
+  const defaultOutput = calculate(defaultInput)
 
   const [output, setOutput] = useState(defaultOutput)
 
   const inputOnChange = e => {
-    const input = sanitize(e.target.value)
+    const input = e.target.value
     const output = calculate(input)
     setOutput(output)
     window.localStorage.setItem('input', input)
